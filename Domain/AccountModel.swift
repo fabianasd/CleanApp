d5b836d7550638191035ce751d696389c258b0b3
Fabiana Petrovick <fafadiaz@hotmail.com>
2021-09-07 14:50:03 -0300

    add AddAccount use case

---
+++ Domain/AccountModel.swift
@@ -0,0 +1,16 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by Macbook on 07/09/21.
+//
+
+import Foundation
+
+
+struct AccountModel {
+    var id: String
+    var name: String
+    var email: String
+    var password: String
+}
--- Domain/AddAccount.swift
+++ Domain/AddAccount.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+//vai receber alguns dados do cadastro, e no final retorna uma conta criada
+protocol AddAccount {
+    func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
+}
+
+struct AddAccountModel {
+    var name: String
+    var email: String
+    var password: String
+    var passwordConfirmation: String
+}
