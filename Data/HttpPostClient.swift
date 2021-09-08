b6929fbcc9e76b44f536f56735224ccafb732abc
Fabiana Petrovick <fafadiaz@hotmail.com>
2021-09-07 22:07:29 -0300

    move production code to their own files

---
+++ Data/HttpPostClient.swift
@@ -0,0 +1,12 @@
+//
+//  HttpPostClient.swift
+//  Data
+//
+//  Created by Macbook on 07/09/21.
+//
+
+import Foundation
+
+public protocol HttpPostClient {
+    func post(to url: URL, with data: Data?)
+}
