711830f1512ef7a60a35a75f33814efefe6c711e
Fabiana Petrovick <fafadiaz@hotmail.com>
2021-09-14 08:07:35 -0300

    move UrlProcolStub to is own file

---
+++ InfraTests/Http/UrlProtocolStub.swift
@@ -0,0 +1,54 @@
+//
+//  UrlProtocolStub.swift
+//  InfraTests
+//
+//  Created by Macbook on 14/09/21.
+//
+
+import Foundation
+
+//generico para qualquer ferramenta que for usada para fazer request (por exemplo a UrlSession)
+class UrlProtocolStub: URLProtocol {
+    static var emit: ((URLRequest) -> Void)?
+    static var data: Data?
+    static var response: HTTPURLResponse?
+    static var error: Error?
+    
+    static func ObserverRequest(completion: @escaping (URLRequest) -> Void) {
+        UrlProtocolStub.emit = completion
+    }
+    
+    static func simulate(data: Data?, response: HTTPURLResponse?, error: Error?) {
+        UrlProtocolStub.data = data
+        UrlProtocolStub.response = response
+        UrlProtocolStub.error = error
+    }
+    // usou override pois esta reescrevendo classe e sobreescrevendo os metodos dela
+    override open class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override open class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override open func startLoading() {
+        UrlProtocolStub.emit?(request)
+        if let data = UrlProtocolStub.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        if let response = UrlProtocolStub.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        if let error = UrlProtocolStub.error {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+        client?.urlProtocolDidFinishLoading(self) // ao terminar dispara o request
+    }
+    
+    override open func stopLoading() {
+        
+    }
+}
+
+
