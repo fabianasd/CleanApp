c98bb2e1935381e2f38e6a3a1e9e91e457471a48
Fabiana Petrovick <fafadiaz@hotmail.com>
2021-09-14 14:19:29 -0300

    move presentation classes to their own files

---
+++ Presentation/AlertView.swift
@@ -0,0 +1,23 @@
+//
+//  AlertView.swift
+//  Presentation
+//
+//  Created by Macbook on 14/09/21.
+//
+
+import Foundation
+
+public protocol AlertView {
+    func showMessage(viewModel: AlertViewModel)
+}
+
+//Model
+public struct AlertViewModel: Equatable {
+    public var title: String
+    public var message: String
+    
+    public init(title: String, message: String) {
+        self.title = title
+        self.message = message
+    }
+}
