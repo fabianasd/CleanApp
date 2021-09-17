87139817d72c26dba0da5a7878d72b2bbd85f8ab
Fabiana Petrovick <fafadiaz@hotmail.com>
2021-09-17 13:38:55 -0300

    add Main target

---
+++ Main/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  Main
+//
+//  Created by Macbook on 17/09/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
