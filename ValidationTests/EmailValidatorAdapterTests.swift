7d97206fb9f1cf41bc18f54058374f191167edb2
Fabiana Petrovick <fafadiaz@hotmail.com>
2021-09-17 08:17:29 -0300

    add Validation target

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,13 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by Macbook on 17/09/21.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+    func test_() {
+    }
+}
