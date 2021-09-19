5abce263e93b2fc0e1133237572fa8de9d88d46f
Fabiana Petrovick <fafadiaz@hotmail.com>
2021-09-19 14:37:17 -0300

    remove unused code

--- MainTests/SignUpIntegrationTests.swift
+++ MainTests/SignUpIntegrationTests.swift
@@ -10,9 +10,6 @@ import Main
 
 class SignUpIntegrationTests: XCTestCase {
     func test_ui_presentation_integration() {
-        debugPrint("======================")
-        debugPrint(Environment.variable(.apiBaseUrl))
-        debugPrint("======================")
         let sut = SignUpComposer.composeControllerWith(addAccount: AddAccountSpy())
         checkMemoryLeak(for: sut)
     }
