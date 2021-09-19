f256f813992addae72505b07490f20b809f28074
Fabiana Petrovick <fafadiaz@hotmail.com>
2021-09-19 16:37:47 -0300

    move threading login

--- Main/Factories/UseCaseFactory.swift
+++ Main/Factories/UseCaseFactory.swift
@@ -17,8 +17,32 @@ final class UseCaseFactory {
     private static func makeUrl(path: String) -> URL {
         return URL(string: "\(apiBaseUrl)/\(path)")!
     }
-
+    
     static func makeRemoteAddAccount() -> AddAccount {
-        return RemoteAddAccount(url: makeUrl(path: "signup"), httpClient: httpClient)
+        let remoteAddAccount = RemoteAddAccount(url: makeUrl(path: "signup"), httpClient: httpClient)
+        return MainQueueDispatchDecorator(remoteAddAccount)
+    }
+}
+
+//Decorator
+public final class MainQueueDispatchDecorator<T> {
+    private let instance: T
+    
+    public init(_ instance: T) {
+        self.instance = instance
+    }
+    
+    func dispatch(completion: @escaping() -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion) }
+        completion()
+    }
+}
+
+extension MainQueueDispatchDecorator: AddAccount where T: AddAccount {
+    public func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, DomainError>) -> Void) {
+        instance.add(addAccountModel: addAccountModel) { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
     }
 }
