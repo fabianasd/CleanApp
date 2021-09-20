12be28ba44f8f1baf0afdaeb206193515b3b2d1d
Fabiana Petrovick <fafadiaz@hotmail.com>
2021-09-20 14:31:08 -0300

    change remove API URL

--- UseCasesIntegrationTests/AddAccountIntegrationTests.swift
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -13,7 +13,7 @@ import Domain
 class AddAccountIntegrationTests: XCTestCase {
     func test_add_account() {
         let alamofireAdapter = AlamofireAdapter()
-        let url = URL(string: "https://clean-node-api.herokuapp.com/api/signup")!
+        let url = URL(string: "https://fordevs.herokuapp.com/api/signup")!
         let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
         let addAccountModel = AddAccountModel(name: "any_name", email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
         let exp = expectation(description: "waiting")
@@ -21,9 +21,7 @@ class AddAccountIntegrationTests: XCTestCase {
             switch result {
             case .failure: XCTFail("Expect success got \(result) instead")
             case .success(let account):
-                XCTAssertNotNil(account.id)
-                XCTAssertEqual(account.name, addAccountModel.name)
-                XCTAssertEqual(account.email, addAccountModel.email)
+                XCTAssertNotNil(account.accessToken)
             }
             exp.fulfill()
         }
