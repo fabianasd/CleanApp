0f488b854a4f49d9ccc996cd516501bc327744cf
Fabiana Petrovick <fafadiaz@hotmail.com>
2021-09-21 10:05:31 -0300

    change Composition to start with WelcomeViewController

--- UI/Components/NavigationController.swift
+++ UI/Components/NavigationController.swift
@@ -10,20 +10,22 @@ import UIKit
 
 //criação da borda em cima 
 public final class NavigationController: UINavigationController {
+    private var currentViewController: UIViewController?
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
-
+    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         setup()
     }
-
+    
     public convenience init() {
         self.init(nibName: nil, bundle: nil)
     }
-
+    
     private func setup() {
         navigationBar.barTintColor = Color.primaryDark
         navigationBar.tintColor = UIColor.white
@@ -31,12 +33,20 @@ public final class NavigationController: UINavigationController {
         navigationBar.isTranslucent = false
         navigationBar.barStyle = .black //inverte as cores das fontes no top do telefone: preto > branco
     }
-
+    
     public func setRootViewController(_ viewController: UIViewController) {
         setViewControllers([viewController], animated: true)
+        currentViewController = viewController
+        hideBackButtonText()
     }
 
-    public func pushViewController(_ viewController: UIViewController) {
+    
+    public func pushViewController(_ viewController: UIViewController) { 
         pushViewController(viewController, animated: true)
+    }
+    
+    public func hideBackButtonText() { // tira o texto explicativo, fica so o "<"
+        currentViewController?.navigationItem.backBarButtonItem = UIBarButtonItem(title: nil, style: .plain, target: nil, action: nil)
+        
     }
 }
