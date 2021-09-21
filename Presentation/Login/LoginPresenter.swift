3e6d0a74a68c1341e3f12213532ea0843db04a64
Fabiana Petrovick <fafadiaz@hotmail.com>
2021-09-20 21:44:03 -0300

    ensure LoginPresenter shows error message if Validation fails

--- Presentation/Login/LoginPresenter.swift
+++ Presentation/Login/LoginPresenter.swift
@@ -10,12 +10,16 @@ import Domain
 
 public final class LoginPresenter {
     private let validation: Validation
+    private let alertView: AlertView
     
-    public init(validation: Validation) {
+    public init(validation: Validation, alertView: AlertView, authentication: Authentication, loadingView: LoadingView) {
         self.validation = validation
+        self.alertView = alertView
     }
     
     public func login(viewModel: LoginRequest) {
-        _ = validation.validate(data: viewModel.toJson())
+        if let message = validation.validate(data: viewModel.toJson()) {
+            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
+        }
     }
 }
