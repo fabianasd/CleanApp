2517bd044f12e34a9f38acddf067739fe4eb24ff
Fabiana Petrovick <fafadiaz@hotmail.com>
2021-09-21 06:39:55 -0300

    ensure LoginViewController implements AlertView protocol

--- UI/Scenes/Login/LoginViewController.swift
+++ UI/Scenes/Login/LoginViewController.swift
@@ -14,14 +14,14 @@ public final class LoginViewController: UIViewController, Storyboarded {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-
+    
     public var login: ((LoginRequest) -> Void)?
-
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
         configure()
     }
-
+    
     private func configure() {
         title = "4Dev"
         loginButton?.layer.cornerRadius = 5
@@ -38,5 +38,13 @@ extension LoginViewController: LoadingView {
             view.isUserInteractionEnabled = true
             loadingIndicator?.stopAnimating()
         }
+    }
+}
+
+extension LoginViewController: AlertView {
+    public func showMessage(viewModel: AlertViewModel) {
+        let alert = UIAlertController(title: viewModel.title, message: viewModel.message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(alert, animated: true)
     }
 }
