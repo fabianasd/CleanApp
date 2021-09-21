597ba4dc85068b0907b0cf8254258500461a5743
Fabiana Petrovick <fafadiaz@hotmail.com>
2021-09-21 06:49:46 -0300

    ensure LoginViewController calls login() when button is tapped

--- UI/Scenes/Login/LoginViewController.swift
+++ UI/Scenes/Login/LoginViewController.swift
@@ -14,18 +14,24 @@ public final class LoginViewController: UIViewController, Storyboarded {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    
+
     public var login: ((LoginRequest) -> Void)?
-    
+
     public override func viewDidLoad() {
         super.viewDidLoad()
         configure()
     }
-    
+
     private func configure() {
         title = "4Dev"
         loginButton?.layer.cornerRadius = 5
+        loginButton?.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
         hideKeyboardOnTap()
+    }
+
+    @objc private func loginButtonTapped() {
+        let viewModel = LoginRequest(email: emailTextField?.text, password: passwordTextField?.text)
+        login?(viewModel)
     }
 }
 
